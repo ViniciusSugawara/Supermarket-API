b9d1bfeff44bca110fecd786025667021767f2db
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-16 00:47:59 -0300

    refactor: Defined relation between ListOfProducts and Product, and mapped classes

--- src/main/java/br/com/supermarketapi/models/ListOfProducts.java
+++ src/main/java/br/com/supermarketapi/models/ListOfProducts.java
@@ -1,9 +1,18 @@
 package br.com.supermarketapi.models;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 
 // Represents a list of products, which will be manipulated by the Client user
-public class ListOfProducts extends BaseEntity {
+@Entity
+public class ListOfProducts{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToMany
+    @JoinTable(name = "products_listofproducts", joinColumns = @JoinColumn(name = "listofproducts_id"),
+            inverseJoinColumns = @JoinColumn(name = "products_id"))
     private List<Product> products;
 
     public List<Product> getProducts() {
--- src/main/java/br/com/supermarketapi/models/Product.java
+++ src/main/java/br/com/supermarketapi/models/Product.java
@@ -1,13 +1,35 @@
 package br.com.supermarketapi.models;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //Represents the product entity, which will be manipulated by the Admin user
-public class Product extends BaseEntity {
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String description;
 
     //Placeholder - Will change to image later
     private String image;
 
+    private boolean status;
+    @ManyToMany
+    @JoinTable(name = "products_listofproducts", joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "listofproducts_id"))
+    private List<ListOfProducts> listOfProducts = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
@@ -30,5 +52,13 @@ public class Product extends BaseEntity {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
