f020b19836b836d472da033c6998982232c52337
ViniciusSugawara <vinicius.sugawara@gmail.com>
2022-12-20 10:28:11 -0300

    refactor: added Repository annotations in product and listofproducts repositories

--- src/main/java/br/com/supermarketapi/repositories/ListOfProductsRepository.java
+++ src/main/java/br/com/supermarketapi/repositories/ListOfProductsRepository.java
@@ -2,6 +2,8 @@ package br.com.supermarketapi.repositories;
 
 import br.com.supermarketapi.models.ListOfProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListOfProductsRepository extends JpaRepository<ListOfProduct, Long> {
 }
--- src/main/java/br/com/supermarketapi/repositories/ProductRepository.java
+++ src/main/java/br/com/supermarketapi/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package br.com.supermarketapi.repositories;
 
 import br.com.supermarketapi.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
